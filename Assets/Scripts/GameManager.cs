9097f225582b64be2b75f4e38130148310e72027
jyj8943 <jyj8943@gmail.com>
2025-07-01 12:43:40 +0900

    2-3 시간 구현하기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text timeTxt;
+    
     public GameObject square;
 
+    private float time = 0f;
+    
     void Start()
     {
         InvokeRepeating("MakingSquare", 0f, 1f);
@@ -13,7 +18,8 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
     }
 
     private void MakingSquare()
